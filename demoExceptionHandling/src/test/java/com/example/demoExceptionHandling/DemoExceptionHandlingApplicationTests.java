faf398c1c58ca0ebbe8ebfc62c9b0425ab77fed3
RaviSoni-ux <67455525+RaviSoni-ux@users.noreply.github.com>
2021-09-22 14:09:35 +0530

    Add files via upload
    Controller Advice
    The @ControllerAdvice is an annotation, to handle the exceptions globally.
    
    Exception Handler
    The @ExceptionHandler is an annotation used to handle the specific exceptions and sending the custom responses to the client.
    
    You can use the following code to create @ControllerAdvice class to handle the exceptions globally −
    
    package com.tutorialspoint.demo.exception;
    
    import org.springframework.web.bind.annotation.ControllerAdvice;
    
    @ControllerAdvice
       public class ProductExceptionController {
    }
    Define a class that extends the RuntimeException class.
    
    package com.tutorialspoint.demo.exception;
    
    public class ProductNotfoundException extends RuntimeException {
       private static final long serialVersionUID = 1L;
    }
    You can define the @ExceptionHandler method to handle the exceptions as shown. This method should be used for writing the Controller Advice class file.
    
    @ExceptionHandler(value = ProductNotfoundException.class)
    
    public ResponseEntity<Object> exception(ProductNotfoundException exception) {
    }
    Now, use the code given below to throw the exception from the API.
    
    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
    public ResponseEntity<Object> updateProduct() { 
       throw new ProductNotfoundException();
    }
    The complete code to handle the exception is given below. In this example, we used the PUT API to update the product. Here, while updating the product, if the product is not found, then return the response error message as “Product not found”. Note that the ProductNotFoundException exception class should extend the RuntimeException.

---
+++ demoExceptionHandling/src/test/java/com/example/demoExceptionHandling/DemoExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoExceptionHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
